2b7e83b2dc8b93fdce3b0b1a368520c1273020e8
Mark Korneliy (home) <Shorc327@mail.ru>
2017-03-12 23:56:41 +0300

    add table

---
+++ src/js/pages/StartPage.js
@@ -0,0 +1,71 @@
+﻿import React from 'react'
+//import _ from 'lodash'
+//import namor from 'namor'
+
+import ReactTable from 'react-table'
+
+export default () => {
+ /* const data = _.map(_.range(5553), d => {
+    return {
+      firstName: namor.generate({ words: 1, numLen: 0 }),
+      lastName: namor.generate({ words: 1, numLen: 0 }),
+      age: Math.floor(Math.random() * 30)
+    }
+  })*/
+  const data = [
+    {
+      age:23,
+        firstName:"flight",
+      lastName:"watch1"
+    },
+    {
+      age:24,
+        firstName:"flight1",
+      lastName:"watch2"
+    },
+    {
+      age:25,
+        firstName:"flight2",
+      lastName:"watch3"
+    },
+    {
+      age:26,
+        firstName:"flight2",
+      lastName:"watch4"
+    }
+  ]
+  const columns = [{
+    header: 'Name',
+    columns: [{
+      header: 'First Name',
+      accessor: 'firstName'
+    }, {
+      header: 'Last Name',
+      id: 'lastName',
+      accessor: d => d.lastName
+    }]
+  }, {
+    header: 'Info',
+    columns: [{
+      header: 'Age',
+      accessor: 'age'
+    }]
+  }]
+
+  return (
+    <div>
+      <div className='table-wrap'>
+        <ReactTable
+          className='-striped -highlight'
+          data={data}
+          columns={columns}
+          defaultPageSize={10}
+        />
+      </div>
+      <div style={{textAlign: 'center'}}>
+        <br />
+        <em>Tip: Hold shift when sorting to multi-sort!</em>
+      </div>
+    </div>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:9090',
+    'webpack/hot/only-dev-server',
+    './src/js/index',
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/assets/js',
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new OpenBrowserPlugin({ url: 'http://localhost:9090' }),
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+    })
+  ],
+
+  module: {
+    loaders: [
+
+
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass',
+      },
+    ]
+  },
+  postcss: [autoprefixer({ browsers: ['last 50 versions'] })]
+
+};
